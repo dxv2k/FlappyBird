8977b12b24705ea8a6db2b8fc86603e0e8d80f99
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-02-20 00:58:19 +0700

    copy src from tutorial, going to refactor the code

--- learn_pygame/main.py
+++ learn_pygame/main.py
@@ -1,11 +1,16 @@
 import pygame, sys
 from pygame.locals import * 
 
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+
 pygame.init()
 
 DISPLAYSURF = pygame.display.set_mode((400,300))
 pygame.display.set_caption('Hello World!')
-
 
 while True: 
     for event in pygame.event.get(): 
