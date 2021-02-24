93b10dd9fc5abe16b1de5d99bebfa55b8e3cc04e
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-02-25 00:48:53 +0700

    add empty args parser, controller for AI and user

---
+++ src/args.py
@@ -0,0 +1,5 @@
+def program_args(): 
+    ''' 
+    '''
+    pass
+
---
+++ src/controller.py
@@ -0,0 +1,17 @@
+def user_controller(): 
+    ''' 
+    TODO: User specific controlling the bird 
+    Not sure what to do here, just figure it out in the process
+    ''' 
+    pass 
+
+def neat_controller(input): 
+    ''' 
+    TODO: train NEAT
+    :param: 
+    input which contains: bird.y, TOP_PIPE position, BOTTOM_PIPE position 
+    output: jump or not 
+    ''' 
+
+
+    return False 
